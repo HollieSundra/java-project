5a2e24477edbac898e51f11bd087a42d6acf9392
Nerdy Birdy <holliesundra@gmail.com>
2023-09-30 23:35:00 -0700

    refinement and config

--- src/main/java/com/hollies/patientapi/model/Patient.java
+++ src/main/java/com/hollies/patientapi/model/Patient.java
@@ -1,7 +1,17 @@
 package com.hollies.patientapi.model;
-import java.util.List;
 
+
+import java.util.List;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
 public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
--- src/main/java/com/hollies/patientapi/repository/PatientRepository.java
+++ src/main/java/com/hollies/patientapi/repository/PatientRepository.java
@@ -1,5 +1,53 @@
 package com.hollies.patientapi.repository;
 
-public class PatientRepository {
-    
+import graphql.kickstart.tools.GraphQLQueryResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PatientResolver implements GraphQLQueryResolver {
+
+    private final PatientRepository patientRepository;
+
+    @Autowired
+    public PatientResolver(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    public List<Patient> findPatientByFirstName(String firstName) {
+        return patientRepository.findPatientByFirstName(firstName);
+    }
+
+     public List<Patient> findPatientByLastName(String lastName) {
+        return patientRepository.findPatientByLastName(lastName);
+    }
+
+     public List<Patient> findPatientByGender(String gender) {
+        return patientRepository.findPatientByGender(gender);
+    }
+
+     public List<Patient> findPatientByPhoneNumber(String phoneNumber) {
+        return patientRepository.findPatientByPhoneNumber(phoneNumber);
+    }
+
+     public List<Patient> findPatientByEmail(String email) {
+        return patientRepository.findPatientByEmail(email);
+    }
+
+     public List<Patient> findPatientByAddress(String address) {
+        return patientRepository.findPatientByAddress(address);
+    }
+
+     public List<Patient> findPatientByVisitDate(String visitDate) {
+        return patientRepository.findPatientByVisitDate(visitDate);
+    }
+
+     public List<Patient> findPatientByDiagnosis(String diagnosis) {
+        return patientRepository.findPatientByDiagnosis(diagnosis);
+    }
+
+     public List<Patient> findPatientByDrugCode(String drugCode) {
+        return patientRepository.findPatientByDrugCode(drugCode);
+    }
+
 }
