ae185b7512b62671b0cbff961015495f99bf85d4
Nerdy Birdy <holliesundra@gmail.com>
2023-09-30 22:51:05 -0700

    inital controller

--- src/main/java/com/hollies/patientapi/controller/GraphQLController.java
+++ src/main/java/com/hollies/patientapi/controller/GraphQLController.java
@@ -1,13 +1,43 @@
 package com.hollies.patientapi.controller;
 
+import org.apache.catalina.connector.Response;
+import org.hibernate.mapping.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import graphql.GraphQL;
+
 @RestController
+@RequestMapping("/graphql")
 public class GraphQLController {
-    
-    @GetMapping(value = "/")
-    public String getPage() {
-        return "Welcome";
+    @Autowired
+    private GraphQL graphQL;
+
+    @PostMapping
+    public ResponseEntity<Object> executeQuery(@RequestBody String query) {
+        ExcutionResult excutionResult;
+
+        try {
+            executionResult = graphQL.execute(query);
+            Map<String, Object> resultMap = new HashMap<>();
+            resultMap.put("data", excutionResult.getData());
+
+            if (!excutionResult.getErrors().isEmpty()) {
+                resultMap.put("errors", excutionResult.getErrors());
+            }
+
+            return ResponseEntity.ok(resultMap);
+        } catch (Exception e) {
+            Map<String, String> errorMap = new HashMap<>();
+            errorMap.put("error", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMap);
+        }
     }
+
 }
---
+++ src/main/java/com/hollies/patientapi/repository/PatientRepository.java
@@ -0,0 +1,5 @@
+package com.hollies.patientapi.repository;
+
+public class PatientRepository {
+    
+}
